cac44f461c80e23661422e4b22c70e04b2a83ef7
Thamires Ramos <thamiresramos014@gmail.com>
2021-06-17 12:02:28 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import Formulario from './Componentes/Formulario';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return ( <div className="App">
+    <h1>Formulário de Endereço</h1>
+    <Formulario aoSalvar={''}/>
+  </div>
+    
   );
 }
 
